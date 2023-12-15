72289c444753f52b0fa8aba6694c1ef818cbc537
Alexandre <alexandrebrosteau@gmail.com>
2023-12-15 07:28:44 +0100

    Remove all javax

--- core/src/main/java/com/abr/formationspring/core/service/DefaultMovieService.java
+++ core/src/main/java/com/abr/formationspring/core/service/DefaultMovieService.java
@@ -3,7 +3,6 @@ package com.abr.formationspring.core.service;
 
 import com.abr.formationspring.core.entity.Movie;
 import com.abr.formationspring.core.repository.MovieRepositoryInterface;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
